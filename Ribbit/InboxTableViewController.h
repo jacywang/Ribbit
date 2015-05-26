7886cbe1e76a65ed655009012a7cadf03672ea4b
Jacy Wang <jacywj@gmail.com>
2015-05-25 22:06:00 -0700

    Add Login and Sign Up scene

--- Ribbit/InboxTableViewController.h
+++ Ribbit/InboxTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  InboxTableViewController.h
 //  Ribbit
 //
 //  Created by JIAN WANG on 5/25/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface InboxTableViewController : UITableViewController
 
 @end
-
